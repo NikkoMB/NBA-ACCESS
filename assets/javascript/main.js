8380cb31253ed84a472cafbefd37f5fa8b0f446e
Nikko Bustillo <>
2018-08-07 09:51:56 -0400

    update8/7

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,123 @@
 //STRICTLY JAVASCRIPT
 "use strict"
 
-alert("Welcome to my NBA page!"); 
+$("#Stephen").on("click", function () {
+    console.log('Steph Curry');
+    var curry = {
+        name: "Stephen Curry",
+        pointsPerGame: "23.1",
+        assistsPerGame: "6.8",
+        stealsPerGame: "1.8"
 
+    }
+})
 
+//------------------------------
+$("#James").on("click", function (){
+    console.log('LeBron James'); 
+    var lebron = {
+        name: "LeBron James",
+        pointsPerGame: "27.2",
+        assitsPerGame: "7.2",
+        stealsPerGame: "1.6"
+
+    }
+}) 
+
+//-----------------------------
+$("#Kevin").on("click", function (){
+    console.log('Kevin Durant');
+    var kevin = {
+        name: "Kevin Durant",
+        pointsPerGame: "27.1",
+        assistsPerGame: "3.9",
+        stealsPerGame: "1.2"
+    }
+})
+
+//-----------------------------
+$("#Anthony").on("click", function (){
+    console.log('Anthony Davis'); 
+    var anthony = {
+        name: "Anthony Davis",
+        pointsPerGame: "23.4",
+        assistsPerGame: "1.9",
+        stealsPerGame: "1.3"
+    }
+})
+
+//------------------------------
+$("#Kawhi").on("click", function () {
+    console.log('Kawhi Leonard');
+    var kawhi = {
+        name: "Kawhi Leonard",
+        pointsPerGame: "",
+        assistsPerGame: "",
+        stealsPerGame: "" 
+        
+    } 
+})
+
+//------------------------------
+$("#Beard").on("click", function () {
+    console.log('James Harden'); 
+    var harden = {
+        name: "",
+        pointsPerGame: "",
+        assistsPerGame: "",
+        stealsPerGame: ""
+    }
+})
+
+//------------------------------
+$("#Russell").on("click", function () {
+    console.log('Russell Westbrook');
+    var westbrook = {
+        name: "",
+        pointsPerGame: "",
+        assistsPerGame: "",
+        stealsPerGame: ""
+    }
+})
+
+$("#Greek").on("click", function () {
+    console.log('Giannis Antetokoumpo'); 
+    var giannis = {
+        name: "",
+        pointsPerGame: "",
+        assistsPerGame: "",
+        stealsPerGame: ""
+    
+    }
+})
+
+$("#Chris").on("click", function () {
+    console.log('Chris Paul'); 
+    var paul = {
+        name: "",
+        pointsPerGame: "",
+        assistsPerGame: "",
+        stealsPerGame: ""
+    }
+})
+
+$("#Joel").on("click", function () {
+    console.log('Joel Embiid'); 
+    var embiid = {
+        name: "",
+        pointsPerGame: "",
+        assistsPerGame: "",
+        stealsPerGame: ""
+    }
+})
+
+$("#Dwyane").on("click", function () {
+    console.log("Dwyane Wade"); 
+    var wade = {
+        name: "",
+        pointsPerGame: "",
+        assistsPerGame: "",
+        stealsPerGame: ""
+    }
+})
 
